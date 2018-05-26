77f9bf4f0049f17fc8d502c95a35c50f14074b0a
Vitaliy Balash <vitalik.balash@gmail.com>
2018-05-26 23:28:53 +0300

    refactor reducers

--- lib/reducers/appReducer.dart
+++ lib/reducers/appReducer.dart
@@ -1,24 +1,8 @@
-import 'package:flutter_todo/actions/todoActions.dart';
+import 'package:flutter_todo/reducers/todoReducer.dart';
 import 'package:flutter_todo/states/addState.dart';
 
 AppState appReducer(AppState oldState, dynamic action) {
-  if (action is ToDoActionCreate) {
-    return oldState.cloneWith(
-      todoState: oldState.todoState.addToDo(action.task),
-    );
-  } else if (action is ToDoActionComplete) {
-    final newToDoList = oldState.todoState.todoList.toList();
-    final elementIndex = newToDoList.indexWhere((item) => item.id == action.id);
-    newToDoList[elementIndex] =
-        newToDoList[elementIndex].copyWith(completed: true);
-    return oldState.cloneWith(
-        todoState: oldState.todoState.copyWith(todoList: newToDoList));
-  } else if (action is ToDoActionDelete) {
-    final newToDoList = oldState.todoState.todoList.toList();
-    final elementIndex = newToDoList.indexWhere((item) => item.id == action.id);
-    newToDoList.removeAt(elementIndex);
-    return oldState.cloneWith(
-        todoState: oldState.todoState.copyWith(todoList: newToDoList));
-  }
-  return oldState;
+  return oldState.cloneWith(
+    todoState: todoReducer(oldState.todoState, action),
+  );
 }
---
+++ lib/reducers/todoReducer.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_todo/actions/todoActions.dart';
+import 'package:flutter_todo/states/todoState.dart';
+
+ToDoState todoReducer(ToDoState oldState, dynamic action) {
+  ToDoState newState = oldState;
+  if (action is ToDoActionCreate) {
+    newState = _handleActionCreate(oldState, action);
+  } else if (action is ToDoActionComplete) {
+    newState = _handleActionComplete(oldState, action);
+  } else if (action is ToDoActionDelete) {
+    newState = _handleActionDelete(oldState, action);
+  }
+  return newState;
+}
+
+ToDoState _handleActionCreate(ToDoState oldState, ToDoActionCreate action) {
+  return oldState.copyWith(
+    todoList: oldState.todoList.toList()..add(new ToDo(action.task)),
+  );
+}
+
+ToDoState _handleActionComplete(ToDoState oldState, ToDoActionComplete action) {
+  final newToDoList = oldState.todoList.toList();
+  final elementIndex = newToDoList.indexWhere((item) => item.id == action.id);
+  newToDoList[elementIndex] =
+      newToDoList[elementIndex].copyWith(completed: true);
+  return oldState.copyWith(todoList: newToDoList);
+}
+
+ToDoState _handleActionDelete(ToDoState oldState, ToDoActionDelete action) {
+  final newToDoList = oldState.todoList.toList();
+  final elementIndex = newToDoList.indexWhere((item) => item.id == action.id);
+  newToDoList.removeAt(elementIndex);
+  return oldState.copyWith(todoList: newToDoList);
+}
